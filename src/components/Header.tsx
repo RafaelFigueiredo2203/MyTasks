c01813e5c12fb1097cb952bc8c59ef7c22ce7703
Rafa Figueiredo <rafaelfigueiredojunior7@gmail.com>
2021-08-01 12:43:55 -0300

    first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import '../styles/header.scss'
+
+export function Header() {
+  return (
+    <header className="header">
+      <div>
+        
+        <h1>To.<strong>do</strong> List</h1>
+      </div>
+    </header>
+  )
+}
